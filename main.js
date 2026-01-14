3f9efd20c8ea1f43158f81241f2182e81f6765cb
Piotr Grzegorczyk <piotr.grzegorczyk.coursedog@gmail.com>
2026-01-14 10:12:09 +0100

    Testing exporting function.

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 // Entry point at repo root that kicks off the app in src
 require('./src/index.js');
 
+function testExportedFunction() {
+  return 'test-exported-function'
+}
+
+module.exports = {
+  testExportedFunction
+}
